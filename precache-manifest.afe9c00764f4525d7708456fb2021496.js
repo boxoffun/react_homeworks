32c60ad7c74a1fffc88800ce6775857b9993d7b9
Aigul <boxoffun96@gmail.com>
2019-11-13 21:36:50 +0200

    Updates

---
+++ precache-manifest.afe9c00764f4525d7708456fb2021496.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cc0393cdbb0804e90bb70191481d8b9",
+    "url": "/react_homeworks/index.html"
+  },
+  {
+    "revision": "3168b427cd2e37d7dce6",
+    "url": "/react_homeworks/static/css/main.2bf821c1.chunk.css"
+  },
+  {
+    "revision": "baf443df28c52e8262a3",
+    "url": "/react_homeworks/static/js/2.b1baf088.chunk.js"
+  },
+  {
+    "revision": "3168b427cd2e37d7dce6",
+    "url": "/react_homeworks/static/js/main.9a517c6b.chunk.js"
+  },
+  {
+    "revision": "1f10d8148030632bd476",
+    "url": "/react_homeworks/static/js/runtime-main.22eff9bd.js"
+  }
+]);
